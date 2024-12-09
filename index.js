3ad965002b6ae0ce7350a83c0130539cc4625c7f
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2024-12-09 18:27:56 +0200

    update 1 for vercel

--- index.js
+++ index.js
@@ -26,17 +26,25 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 // Enable other domains to access your application
 app.use(cors());
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
+  console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
 // Mount Routes
 mountRoutes(app);
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello World!" });
+});
+
+app.all("*", (req, res, next) => {
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
+});
 
 // Global error handling middleware for express
 app.use(globalError);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
   console.log(`App running running on port ${PORT}`);
 });
