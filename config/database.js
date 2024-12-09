8b246983d0c114c46f80655f7ee82868b360a0b5
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2024-12-09 21:19:02 +0200

    fix errors 4

--- config/database.js
+++ config/database.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const dbConnection = () => {
   mongoose
-    .connect(process.env.DB_URI, { dbName: "Movies" })
+    .connect(process.env.DB_URI, {
+      dbName: "Movies",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then((conn) => {
       console.log(`Database Connected ${conn.connection.host}`);
     });
